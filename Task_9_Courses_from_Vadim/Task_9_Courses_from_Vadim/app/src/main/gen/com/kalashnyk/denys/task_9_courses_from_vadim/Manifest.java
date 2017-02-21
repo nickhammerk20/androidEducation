64dd9e1a109ce959eea9496088bda84d240033c5
nickhammerk20 <kolich.x10@gmail.com>
2017-02-21 19:43:51 +0200

    deleted double code

--- Task_9_Courses_from_Vadim/Task_9_Courses_from_Vadim/app/src/main/gen/com/kalashnyk/denys/task_9_courses_from_vadim/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.kalashnyk.denys.task_9_courses_from_vadim;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
