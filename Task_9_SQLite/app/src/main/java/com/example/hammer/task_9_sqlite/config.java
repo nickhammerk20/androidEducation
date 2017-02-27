d6553323731de4956eaf89633885c299805f29de
nickhammerk20 <kolich.x10@gmail.com>
2017-02-27 19:33:00 +0200

    SQL create - commit

--- Task_9_SQLite/app/src/main/java/com/example/hammer/task_9_sqlite/config.java
+++ Task_9_SQLite/app/src/main/java/com/example/hammer/task_9_sqlite/config.java
@@ -4,11 +4,16 @@ package com.example.hammer.task_9_sqlite;
  * Created by hammer on 22.02.2017.
  */
 
+
+//Создание структуры таблицы
+
+
 public class config {
-    public static final String DB_NAME = "Person Data Base";
+    public static final String DB_NAME = "Person Data Base";            // Имя базы
 
-    public static final String TABLE_PERSON = "Person";
+    public static final String TABLE_PERSON = "Person";                 // Имя таблицы
 
+    //Перечисление столбцов, присвоение имен
     public static final String KEY_ID = "Id";
     public static final String KEY_NAME = "Name";
     public static final String KEY_SURENAME = "Surename";
@@ -16,8 +21,10 @@ public class config {
     public static final String KEY_MAIL = "Mail";
     public static final String KEY_SKYPE = "Skype";
 
+    // массив столбцов
     public static final String[] COLUMNS = {KEY_ID, KEY_NAME, KEY_SURENAME, KEY_PHONE, KEY_MAIL, KEY_SKYPE};
 
+    // Формирование строки команды SQL для создания тьаблицы
     public static final String COMMAND_CREATE = "create table"
             + TABLE_PERSON + " ("
             + KEY_ID + "INTEGER PRIMARY KEY, "
@@ -28,7 +35,12 @@ public class config {
             + KEY_SKYPE + " TEXT "
             + ");";
 
+    // формирование строки команды  SQL для  удаления таблицы
     public static final String COMMAND_DELETE = "DROP TABLE IF EXIST " + TABLE_PERSON;
+
+    // выбор таблицы
     public static final String COMMAND_SELECT = "SELECT * FROM " + TABLE_PERSON;
+
+    // версия базы. Обязательная ИНТОВАЯ переменная
     public static final int DB_VERSION = 210217;
 }
