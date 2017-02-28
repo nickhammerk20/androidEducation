5e4cb928e1cb64abcdfd12fd961e4ca9fda1c1b2
nickhammerk20 <kolich.x10@gmail.com>
2017-02-28 20:50:26 +0200

    WTF???!!!

--- Task_11_File_Camera/app/src/main/java/com/example/hammer/task_11_file_camera/MainActivity.java
+++ Task_11_File_Camera/app/src/main/java/com/example/hammer/task_11_file_camera/MainActivity.java
@@ -1,13 +1,138 @@
 package com.example.hammer.task_11_file_camera;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int REQEST_CAMERA = 0, SELECT_FILE = 1;
+    private Button btnSelect;
+    private ImageView ivImage;
+//    private String userChooserTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSelect = (Button) findViewById(R.id.btn_select_photo);
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+        ivImage = (ImageView) findViewById(R.id.iv_image);
+    }
+
+    private void selectImage() {
+        final CharSequence[] items = {  "Take Photo",
+                                        "Chose from Library",
+                                        "Cancel"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Add photo!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (items[which].equals("Take Photo"))
+                {
+//                    userChooserTask = "Take Photo";
+                    cameraIntent();
+                }
+                else if (items[which].equals("Chose from Library"))
+                {
+//                    userChooserTask = "Chose from Library";
+                    galleryIntent();
+                }
+                else if (items[which].equals("Cancel"))
+                {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    private void galleryIntent()
+    {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent , "Select File"), SELECT_FILE);
+    }
+
+    private void cameraIntent()
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQEST_CAMERA);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == Activity.RESULT_OK)
+        {
+            if(requestCode == SELECT_FILE)
+                onSelectFromGalleryResult(data);
+            else if (requestCode == REQEST_CAMERA)
+                onGalleryImageResult(data);
+        }
+    }
+
+    private void onGalleryImageResult(Intent data)
+    {
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90 , bytes);
+
+        File destination = new File(Environment.getExternalStorageDirectory(),
+//                "TASK_11_"+
+                        System.currentTimeMillis() + ".jpg");
+        FileOutputStream fo;
+
+        try {
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ivImage.setImageBitmap(thumbnail);
+    }
+
+    private void onSelectFromGalleryResult(Intent data)
+    {
+        Bitmap bm = null;
+        if(data != null)
+        {
+            try {
+                bm = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        ivImage.setImageBitmap(bm);
     }
 }
