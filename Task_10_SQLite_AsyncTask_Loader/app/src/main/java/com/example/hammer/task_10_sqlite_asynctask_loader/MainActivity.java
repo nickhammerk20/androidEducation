29e29259b0deede365a602b6fd49f460e8eb2230
nickhammerk20 <kolich.x10@gmail.com>
2017-03-02 15:54:37 +0200

    Start new Project. HW_10

---
+++ Task_10_SQLite_AsyncTask_Loader/app/src/main/java/com/example/hammer/task_10_sqlite_asynctask_loader/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hammer.task_10_sqlite_asynctask_loader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
