aee4463efc4b4e92a7a5d9ff64ff25db2113d20e
nickhammerk20 <kolich.x10@gmail.com>
2017-03-02 21:49:50 +0200

    21:50

--- Task_10_SQLite_AsyncTask_Loader/app/src/main/java/com/example/hammer/task_10_sqlite_asynctask_loader/ui/PersonDetailActivity.java
+++ Task_10_SQLite_AsyncTask_Loader/app/src/main/java/com/example/hammer/task_10_sqlite_asynctask_loader/ui/PersonDetailActivity.java
@@ -1,11 +1,28 @@
 package com.example.hammer.task_10_sqlite_asynctask_loader.ui;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.hammer.task_10_sqlite_asynctask_loader.R;
 
 /**
  * Created by hammer on 02.03.2017.
  */
 
 public class PersonDetailActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
 
+        TextView tvIdDetailsPerson = (TextView) findViewById(R.id.text_view_details_id);
+        TextView tvNameDetailsPerson = (TextView) findViewById(R.id.text_view_details_name);
+        TextView tvSurnameDetailsPerson = (TextView) findViewById(R.id.text_view_details_surname);
+        TextView tvPhoneDetailsPerson = (TextView) findViewById(R.id.text_view_details_phone);
+        TextView tvSkypeDetailsPerson = (TextView) findViewById(R.id.text_view_details_skype);
+        TextView tvMailDetailsPerson = (TextView) findViewById(R.id.text_view_details_mail);
+
+    }
 }
--- Task_10_SQLite_AsyncTask_Loader/app/src/main/java/com/example/hammer/task_10_sqlite_asynctask_loader/ui/PersonsListActivity.java
+++ Task_10_SQLite_AsyncTask_Loader/app/src/main/java/com/example/hammer/task_10_sqlite_asynctask_loader/ui/PersonsListActivity.java
@@ -1,8 +1,11 @@
 package com.example.hammer.task_10_sqlite_asynctask_loader.ui;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
+import com.example.hammer.task_10_sqlite_asynctask_loader.R;
 import com.example.hammer.task_10_sqlite_asynctask_loader.adapter.MyAdapter;
 import com.example.hammer.task_10_sqlite_asynctask_loader.db.CRUDSQLite;
 import com.example.hammer.task_10_sqlite_asynctask_loader.model.Person;
@@ -18,5 +21,18 @@ public class PersonsListActivity extends AppCompatActivity {
     private ArrayList<Person> persons;
     private CRUDSQLite crudsqLite;
     private MyAdapter myAdapter;
-    
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_persons_list);
+        lvPersons = (ListView) findViewById(R.id.list_view_persons_list);
+        persons = new ArrayList<>();
+        crudsqLite = new CRUDSQLite(this);
+        persons = crudsqLite.getAllPerson();
+        myAdapter = new MyAdapter(this, persons);
+        lvPersons.setAdapter(myAdapter);
+    }
+
 }
