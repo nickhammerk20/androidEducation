278dc428a13002ab589d2de4d9ede3b6f0693119
nickhammerk20 <kolich.x10@gmail.com>
2017-03-02 19:25:21 +0200

    - add item
    - MainActivity

--- Task_10_SQLite_AsyncTask_Loader/app/src/main/java/com/example/hammer/task_10_sqlite_asynctask_loader/MainActivity.java
+++ Task_10_SQLite_AsyncTask_Loader/app/src/main/java/com/example/hammer/task_10_sqlite_asynctask_loader/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.hammer.task_10_sqlite_asynctask_loader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText etName, etSurname, etPhone, etSkype, etMail;
+    private Button btnSave;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etName = (EditText) findViewById(R.id.edit_text_main_name);
+        etSurname = (EditText) findViewById(R.id.edit_text_main_surname);
+        etPhone = (EditText) findViewById(R.id.edit_text_main_phone_number);
+        etSkype = (EditText) findViewById(R.id.edit_text_main_skype);
+        etMail = (EditText) findViewById(R.id.edit_text_main_mail);
+
+        btnSave = (Button) findViewById(R.id.btn_main_save);
+        btnSave.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
---
+++ Task_10_SQLite_AsyncTask_Loader/app/src/main/java/com/example/hammer/task_10_sqlite_asynctask_loader/PersonDetailActivity.java
@@ -0,0 +1,11 @@
+package com.example.hammer.task_10_sqlite_asynctask_loader;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hammer on 02.03.2017.
+ */
+
+public class PersonDetailActivity extends AppCompatActivity{
+
+}
